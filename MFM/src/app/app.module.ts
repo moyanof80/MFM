30b46bb3b1801326f289a00332e50b93a7fe7bf3
Fernanda <moyanof80@gmail.com>
2022-07-24 11:25:35 -0300

    Arreglando Header

--- MFM/src/app/app.module.ts
+++ MFM/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
-import { HysComponent } from './componentes/hys/hys.component';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +22,7 @@ import { HysComponent } from './componentes/hys/hys.component';
     AcercaDeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HysComponent
+
   ],
   imports: [
     BrowserModule,
